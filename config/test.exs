822dd21f2130873fe29508119bce2026f235d486
Arjan Scherpenisse <arjan@miraclethings.nl>
2023-03-09 11:50:45 +0100

    Fix test

--- config/test.exs
+++ config/test.exs
@@ -5,7 +5,9 @@ import Config
 # The MIX_TEST_PARTITION environment variable can be used
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
-config :knit_maker, KnitMaker.Repo, database: "test.db"
+config :knit_maker, KnitMaker.Repo,
+  database: "test.db",
+  pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
