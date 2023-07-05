74b39f614b5bb11995f17baee847b0842e87ef45
Arjan Scherpenisse <arjan@miraclethings.nl>
2023-07-05 12:47:38 +0200

    Add bw=1 parameter for bw image download

--- lib/knit_maker_web/controllers/image_controller.ex
+++ lib/knit_maker_web/controllers/image_controller.ex
@@ -6,15 +6,25 @@ defmodule KnitMakerWeb.ImageController do
   def render(conn, params) do
     event = Events.get_event!(params["id"])
 
+    {fg, bg} = colors(event, params)
+
     {:ok, image_data} =
       event
       |> KnitMaker.Visualizer.render(params["participant_id"])
-      |> Pat.to_pixels(%{"1" => event.knitting_fg, "0" => event.knitting_bg})
+      |> Pat.to_pixels(%{"1" => fg, "0" => bg})
       |> Pixels.encode_png()
 
     conn
     |> put_resp_content_type("image/png")
     |> put_resp_header("content-disposition", "inline; filename=knit.png")
     |> send_resp(200, image_data)
+  end
+
+  defp colors(_event, %{"bw" => "1"}) do
+    {"#ffffff", "#000000"}
+  end
+
+  defp colors(event, _params) do
+    {event.knitting_fg, event.knitting_bg}
   end
 end
